3d40355e5061b9eb1fa37c2f8f1b17b1d63475e5
nnamb <namdinantambu1@gmail.com>
2026-02-10 17:48:17 -0500

    broken will fix later

--- app/src/main/java/com/example/mycontactlist/ContactListActivity.java
+++ app/src/main/java/com/example/mycontactlist/ContactListActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -18,6 +21,7 @@ import java.util.ArrayList;
 
 public class ContactListActivity extends AppCompatActivity {
     ArrayList<Contact> contacts;
+    ContactAdapter contactAdapter;
     private View.OnClickListener onItemClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -26,6 +30,7 @@ public class ContactListActivity extends AppCompatActivity {
             int position = viewHolder.getAdapterPosition();
             int contactID = contacts.get(position).getContactID();
             Intent intent = new Intent(ContactListActivity.this, MainActivity.class);
+            intent.putExtra("contactID", contactID);
             startActivity(intent);
         }
     };
@@ -40,10 +45,7 @@ public class ContactListActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
         ContactDataSource ds = new ContactDataSource(this);
-        ArrayList<Contact> contacts;
-
         try {
             ds.open();
             contacts = ds.getContacts();
@@ -51,21 +53,33 @@ public class ContactListActivity extends AppCompatActivity {
             RecyclerView contactList = findViewById(R.id.rvContacts);
             RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
             contactList.setLayoutManager(layoutManager);
-            ContactAdapter contactAdapter = new ContactAdapter(contacts);
+            ContactAdapter contactAdapter = new ContactAdapter(contacts, this);
             contactList.setAdapter(contactAdapter);
         }
         catch (Exception e){
             Toast.makeText(this, "Error retrieving contacts", Toast.LENGTH_SHORT).show();
         }
-        initAdContactButton();
+        initAddContactButton();
+        initDeleteSwitch();
     }
-    private void initAdContactButton(){
+    private void initAddContactButton(){
         Button newContact = findViewById(R.id.buttonAddContact);
         newContact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ContactListActivity.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+    }
+    private void initDeleteSwitch(){
+        Switch ds = findViewById(R.id.switchDelete);
+        ds.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
+                Boolean status = compoundButon.isChecked();
+                contactAdapter.setDelete(status);
+                contactAdapter.notifyDataSetChnaged();
             }
         });
     }
